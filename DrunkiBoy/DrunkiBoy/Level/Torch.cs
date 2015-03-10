6823556c971b5eb837a56fda714bdd56d178c2d3
Morozbea <morozbea@freemail.hu>
2015-03-10 15:50:26 +0100

    tagit bort onödiga torchbilder

--- DrunkiBoy/DrunkiBoy/Level/Torch.cs
+++ DrunkiBoy/DrunkiBoy/Level/Torch.cs
@@ -21,8 +21,7 @@ namespace DrunkiBoy
         //Kan ta bort Draw() härifrån sen när Torch är implementerad, eftersom den använder Draw i AnimatedObject klassen
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin();
-            //spriteBatch.Draw(tex, pos, Color.White);
+            spriteBatch.Begin();           
             base.Draw(spriteBatch);
             spriteBatch.End();
         }
