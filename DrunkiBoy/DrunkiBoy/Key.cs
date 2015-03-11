e29e922b370c627c705b30d5f660d32e1c748300
Morozbea <morozbea@freemail.hu>
2015-03-11 20:27:29 +0100

    Keyklass
    funkar att lägga ut flera i editor
    men
    -det inte animeras (måste kolla orsaken)
    -och varför det inte syns i ingame

---
+++ DrunkiBoy/DrunkiBoy/Key.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DrunkiBoy
+{
+    class Key : AnimatedObject
+    {
+         public Key(Vector2 pos, Texture2D tex, Rectangle srcRect, bool isActive, int nrFrames, double frameInterval)
+            : base(pos, tex, srcRect, isActive, nrFrames, frameInterval)
+        {
+            
+            this.type = "Key";            
+           
+        }
+    }
+}
