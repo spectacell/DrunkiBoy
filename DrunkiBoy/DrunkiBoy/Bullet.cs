df565bb5910af7d37a3fa3bd7e2348fd9c230933
Morozbea <morozbea@freemail.hu>
2015-03-31 23:07:52 +0200

    klasser
    påbörjat : bullet, bulletmanager, particle , particleEngine klasser som
    är inte helt färdiga än har mycket att göra.
    spelet kan startas, allt är utkommenterad
    vet kanske känns som kaos men hoppas att vi kan lägga ihop allt :)

---
+++ DrunkiBoy/DrunkiBoy/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace DrunkiBoy
+{
+    //här är jag  osäker om vi ska ärva från gameobject eller hur jag ska fortsätta
+    class Bullet // : GameObject
+    {
+
+    //    public Bullet(Texture2D texture, Vector2 position)
+    //        : base(texture, position)
+    //    {
+
+    //    }
+    }
+}
---
+++ DrunkiBoy/DrunkiBoy/BulletManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+namespace DrunkiBoy
+{
+    static class BulletManager
+    {
+        public static Texture2D tex;
+        public static List<Bullet> BulletLista = new List<Bullet>();
+        public static ParticleEngine particleEngine;
+
+        public static void AddBullet(Enemy target, Player player)
+        {
+            //här ska vi skriva in de olika bullet som hamburgare, alkohol  osv men vi behöver att skaffa de olika "bullet"klasserna
+            // som t.ex. : if ( hamburgare is hamburgareklass)
+            //                  { Bullet = new hamburgareklass                 
+            //                  }
+            //                  else Bullet = new alkoholklass....
+            //Bullet Bullet;
+           // Bullet = new Bullet(tex, target.pos);     
+           //BulletLista.Add(Bullet);
+        }
+        public static void Update(GameTime gameTime)
+        {
+
+        }
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Bullet b in BulletLista)
+            {
+                //b.Draw(spriteBatch);          b.Draw går itne att skriva...
+            }
+            particleEngine.Draw(spriteBatch);
+        }
+    }
+}
