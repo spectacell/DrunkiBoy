dd3f4190fb86aba81f377b0a68c807de9723e36c
Morozbea <morozbea@freemail.hu>
2015-04-12 15:48:39 +0200

    ändringar
    småändringar till orginal efter lite försök i particle engine

--- DrunkiBoy/DrunkiBoy/ParticleEngine.cs
+++ DrunkiBoy/DrunkiBoy/ParticleEngine.cs
@@ -11,6 +11,7 @@ namespace DrunkiBoy
     public class ParticleEngine
     {
         public bool isActive;
+        public bool isAktivInCircle;
         Texture2D colorTexture; //Textur att slumpa färger ur
         Color[] colors;
         private Random random; // random number generator
@@ -20,7 +21,7 @@ namespace DrunkiBoy
         public ParticleEngine(List<Texture2D> textures, Vector2 location, Texture2D colorTexture, bool isActive)
         {
             EmitterLocation = location;
-            this.isActive = isActive;
+            this.isActive = isActive;            
             this.colorTexture = colorTexture;
             this.textures = textures;
             this.particles = new List<Particle>();
@@ -58,30 +59,59 @@ namespace DrunkiBoy
             int ttl = 40 + random.Next(40);
             return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
         }
-        
+        private Particle GenerateParticleCircleRange()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                    1f * (float)(random.NextDouble() * 2 - 1),
+                    1f * (float)(random.NextDouble() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(random.NextDouble() * 2 - 1);
+            Color color = new Color(
+                    (float)random.NextDouble(),
+                    0,
+                    0);
+            float size = (float)(random.NextDouble() * 0.5);
+            int ttl = 40 + random.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+        }
+        internal void CreateParticlesInCircleRange(Vector2 position)
+        {
+            int total = 100;
+            EmitterLocation = position;
+
+            for (int i = 0; i < total; i++)
+            {
+                particles.Add(GenerateParticleCircleRange());
+            }
+        }
 
         public void Update(Vector2 pos)
         {
-            if (isActive) 
-            { 
+            if (isActive)
+            {
                 EmitterLocation = pos;
                 int total = 20;
-            
+
                 for (int i = 0; i < total; i++)
-			    {
-                   particles.Add(GenerateNewParticle());
-                   
-			    }
-			    for (int particle = 0; particle < particles.Count; particle++)
-			    {
-			        particles[particle].Update();
+                {
+                    particles.Add(GenerateNewParticle());
+                    // particles.Add(GenerateParticleCircleRange());
+
+                }
+                for (int particle = 0; particle < particles.Count; particle++)
+                {
+                    particles[particle].Update();
                     if (particles[particle].TTL <= 0)
                     {
                         particles.RemoveAt(particle);
                         particle--;
                     }
-			    }
+                }
             }
+            
         }
         public void Draw(SpriteBatch spriteBatch)
         {
