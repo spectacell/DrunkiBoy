481ade316bf43d3945abd6c566adc99aa5b80c4f
spectacell <spectacell@gmail.com>
2015-04-23 09:29:46 +0200

    Förenklat pizzaweaponkoden

--- DrunkiBoy/DrunkiBoy/Level/Bullets/PizzaWeapon.cs
+++ DrunkiBoy/DrunkiBoy/Level/Bullets/PizzaWeapon.cs
@@ -9,40 +9,21 @@ namespace DrunkiBoy
 {
     class PizzaWeapon : Bullet
     {
-        private bool hasTurned;
         float speed = 70;
-        public static ParticleEngine particleEngine;
         public PizzaWeapon(Vector2 pos, Vector2 velocity)
             : base(pos, velocity, Textures.pizza, true, 450)
         {
-            
             this.velocity.Y = -2; //Så att går lite snett uppåt
         }
         public override void Update(GameTime gameTime)
         {
             pos += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds * speed;
             pos.Y += 0.3f; //gravitation...
-            if (!hasTurned)
-            {
-                speed -= (float)Math.Pow(4, 2) / 10; //Minskar farten mer och mer tills den vänder
-            }
-            else
-            { 
-                speed += (float)Math.Pow(4, 2) / 10; //Ökar farten efter att den vänt
-            }
+            speed -= (float)Math.Pow(4, 2) / 10; //Minskar farten mer och mer tills den vänder
+
             rotation += 0.1f;
             shotRange = shotRange - velocity.Length();
-
-            if (shotRange <= 0 && !hasTurned)
-            {
-                velocity *= -1;
-                hasTurned = true;
-                shotRange = 400;
-            }
-            if (hasTurned)
-            {
-                DeactivateIfOutOfBounds(new Rectangle(0, 0, Level.levelWidth, Level.levelHeight));
-            }
+            DeactivateIfOutOfBounds(new Rectangle(0, 0, Level.levelWidth, Level.levelHeight));
           
            
         }
