62fa6dae6cba0901d5b3637da033869c1c208c36
spectacell <spectacell@gmail.com>
2015-05-05 09:14:16 +0200

    bugfixar player-texturbyten vid upplockande av powerups

--- DrunkiBoy/DrunkiBoy/Level/Player.cs
+++ DrunkiBoy/DrunkiBoy/Level/Player.cs
@@ -14,7 +14,7 @@ namespace DrunkiBoy
         public bool movingBack, weaponThrown;
 
         //LB = Lower Body. För att kunna animera benen för sig så att player inte springer på stället när man kör skjutanimationen
-        private Texture2D texUpperBody, texLowerBody, prevTexUpperBody, prevTexUpperBodyPU;
+        private Texture2D texUpperBody, texLowerBody, prevTexUpperBody, texUpperBodyWithoutPowerUp;
         private double timeTilNextFrameLB = 0; 
         private int frameLB;
         private Rectangle srcRectLB;
@@ -347,12 +347,12 @@ namespace DrunkiBoy
         public void ActivatePowerUp(int powerUp, double time) 
         {
             isMorphing = true;
-            DeactivatePowerUp();
+            //DeactivatePowerUp();
             activePowerUp = powerUp;
             activePowerUpTimer = time;
             Game1.gui.ShowPowerUpCounter(powerUp, time);
             //if (activePowerUp == 0)
-            prevTexUpperBodyPU = texUpperBody;
+            texUpperBodyWithoutPowerUp = texUpperBody;
             
             //Ser till att frame sätts till första framen i animationen
             if (facing == 0) //Vänd åt vänster
@@ -541,50 +541,50 @@ namespace DrunkiBoy
             switch (type)
             {
                 case weaponType.none:
-                    prevTexUpperBody = Textures.player_upper_body;
+                    texUpperBodyWithoutPowerUp = Textures.player_upper_body;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.none;
                     break;
                 case weaponType.burger:
-                    prevTexUpperBody = Textures.player_burger;
+                    texUpperBodyWithoutPowerUp = Textures.player_burger;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.burger;
                     break;
                 case weaponType.pizza:
-                    prevTexUpperBody = Textures.player_pizza;
+                    texUpperBodyWithoutPowerUp = Textures.player_pizza;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.pizza;
                     break;
                 case weaponType.kebab:
-                    prevTexUpperBody = Textures.player_kebab;
+                    texUpperBodyWithoutPowerUp = Textures.player_kebab;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.kebab;
                     break;
                 case weaponType.bottle:
-                    prevTexUpperBody = Textures.player_bottle;
+                    texUpperBodyWithoutPowerUp = Textures.player_bottle;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.bottle;
                     break;
                 case weaponType.molotovCocktail:
-                    prevTexUpperBody = Textures.player_bottle_molotov;
+                    texUpperBodyWithoutPowerUp = Textures.player_bottle_molotov;
                     if (activePowerUp == 0)
                     {
-                        texUpperBody = prevTexUpperBody;
+                        texUpperBody = texUpperBodyWithoutPowerUp;
                     }
                     currentWeapon = weaponType.molotovCocktail;
                     break;
@@ -601,7 +601,8 @@ namespace DrunkiBoy
                 shotDelay = shotDelayDefault;
                 Vector2 bulletPos, bulletVelocity;
                 prevTexUpperBody = texUpperBody;
-                switch (activePowerUp)
+                //Skjutanimationer
+                switch (activePowerUp) 
                 {
                     case 0:
                         texUpperBody = Textures.player_shooting;
@@ -609,10 +610,6 @@ namespace DrunkiBoy
 
                     case 1:
                         texUpperBody = Textures.player_invincible_shooting;
-                        break;
-
-                    case 2:
-
                         break;
                 }
                 animateShooting = true;
@@ -637,7 +634,9 @@ namespace DrunkiBoy
                     case weaponType.pizza:
                         BulletManager.AddBullet(new PizzaWeapon(bulletPos, bulletVelocity, false, true));
                         if (activePowerUp == 0)
-                            prevTexUpperBody = Textures.player_upper_body;                 
+                            prevTexUpperBody = Textures.player_upper_body;  
+                        else
+                            texUpperBodyWithoutPowerUp = Textures.player_upper_body;
                         currentWeapon = weaponType.none;
                     break;
 
@@ -675,7 +674,10 @@ namespace DrunkiBoy
                 if ((facing == 1 && frame == 7) || (facing == 0 && frame == 15) || (facing != prevFacing))
                 {
                     animateShooting = false;
-                    texUpperBody = prevTexUpperBody; //Byter tillbaka till föregående textur så att skottanimationen bara körs en gång per skott
+                    if (activePowerUp == 0 && currentWeapon != weaponType.none) //Lite fulkod med weaponType.none så att pizza-texturen inte visas när man kastat iväg den
+                        texUpperBody = texUpperBodyWithoutPowerUp; //Byter tillbaka till föregående textur så att skottanimationen bara körs en gång per skott
+                    else
+                        texUpperBody = prevTexUpperBody;
                 }
             }
         }
@@ -770,13 +772,14 @@ namespace DrunkiBoy
         /// </summary>
         private void DeactivatePowerUp()
         {
-            if (activePowerUp != 0) { 
+            if (activePowerUp != 0) 
+            { 
                 rotation = 0;
                 particleEngine.isActive = false;
                 invincible = false;
                 activePowerUp = 0;
                 Game1.gui.ResetPowerUp();
-                texUpperBody = prevTexUpperBodyPU;
+                texUpperBody = texUpperBodyWithoutPowerUp;
             }
         }
         #endregion
